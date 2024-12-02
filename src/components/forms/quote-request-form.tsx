485103e5cfa404e4414141286bf79362b60a1175
infinitydev4studio <infinitydev4studio@gmail.com>
2024-12-02 11:40:16 +0100

    fix build

--- src/components/forms/quote-request-form.tsx
+++ src/components/forms/quote-request-form.tsx
@@ -2,7 +2,7 @@
 
 'use client'
 
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
